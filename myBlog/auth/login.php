4793ce9ab266f7f48177912c7d373e60e3668e4a
xd59 <xd59@njit.edu>
2021-06-03 15:44:11 -0400

    update

--- myBlog/auth/login.php
+++ myBlog/auth/login.php
@@ -12,13 +12,18 @@ if(mysqli_num_rows($query) > 0) {
         
     if($row['password'] == $pass) { // log in successful
         echo true;
+        session_start();
+        $_SESSION['user'] = $name;
+        header("Location: ../page2.php");
     }
     else {
         echo false;
-    }    
+        echo "wrong pass";
+    }
 }
 else {
-    echo false;        
+    echo false;
+    echo "no such user";
 }
 ?>
 
